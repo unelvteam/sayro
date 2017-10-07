9ac191971d3d79a7015dc03b1fc49547fabb9a07
unelvteam <unelvteam@users.noreply.github.com>
2017-10-07 13:48:45 +0300

    intit commit
    start project

---
+++ New project/Assets/heart_hud.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heart_hud : MonoBehaviour {
+
+
+    public int lives;
+    public Texture live_texture;
+    public int live_x;
+    public int live_y;
+	// Use this for initialization
+	void Start () {
+        lives = 3;
+        live_x = 10;
+        live_y = 10;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetKeyUp(KeyCode.Z))
+        {
+            lives++;
+        }
+
+        if (Input.GetKeyUp(KeyCode.X))
+        {
+            lives--;
+        }
+    }
+
+    private void OnGUI()
+    {
+        for (int i = 0; i < lives; i++)
+        {
+            GUI.DrawTexture(new Rect(live_x + i * 64, live_y, 64, 64), live_texture);
+        }
+    }
+}
